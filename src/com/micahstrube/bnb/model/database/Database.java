de084514c864d288127a45c12905969c98a8c247
Micah Strube <micah.strube@me.com>
2017-10-29 08:41:20 -0400

    Created .sql file to create tables.

--- src/com/micahstrube/bnb/model/database/Database.java
+++ src/com/micahstrube/bnb/model/database/Database.java
@@ -1,18 +1,18 @@
 package com.micahstrube.bnb.model.database;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 class Database {
-    String username;
-    String password;
-    String databaseName;
-
-    public Connection connect(String url, String user, String pass) throws SQLException {
-        Connection conn = null;
+    public static Connection connect(String directory, String databaseName, String user, String pass)
+            throws SQLException {
+        Connection conn;
         Properties connectionProps = new Properties();
-        connectionProps.put("user", this.username);
-        connectionProps.put("password", this.password);
-        conn = DriverManager.getConnection("jdbc:derby:" + this.databaseName + ";create=true", connectionProps);
+        connectionProps.put("user", user);
+        connectionProps.put("password", pass);
+        conn = DriverManager
+                .getConnection("jdbc:derby:" + directory + ":" + databaseName + ";create=true", connectionProps);
 
         System.out.println("Connected to database " + databaseName);
         return conn;

b31c3a7436d85889e6dc1ebb5732feaa805a9d72
Micah Strube <micah.strube@me.com>
2017-12-29 13:50:05 -0500

    Now using FXML to set up the GUI

--- src/com/micahstrube/bnb/Controller.java
+++ src/com/micahstrube/bnb/Controller.java
@@ -1,4 +1,14 @@
 package com.micahstrube.bnb;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.text.Text;
+
 public class Controller {
+    @FXML private TabPane tabPane;
+    @FXML private Tab reservationTab;
+    @FXML private Tab guestTab;
+    @FXML private Tab roomTab;
+    @FXML private Text testText;
 }
--- src/com/micahstrube/bnb/Main.java
+++ src/com/micahstrube/bnb/Main.java
@@ -1,23 +1,46 @@
 package com.micahstrube.bnb;
 
+import com.micahstrube.bnb.model.database.Database;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        Scene scene = new Scene(root, 800, 600,  Color.WHITE);
+        primaryStage.setTitle("BNB");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        Connection db = null;
+        try {
+            db = Database.connectOrCreate("bnb-db", "test", "test");
+
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to create database. Exiting.");
+            System.exit(1);
+        }
+
+        System.out.println("Launching GUI");
         launch(args);
+
+        try {
+            db.close();
+        } catch (SQLException e) {
+            // Do nothing
+        }
+
     }
 }
